9432dc2e41b11f1f6a45c563c9abc27b9f06ef8b
Cleberton Kobay <Clebertonkobay@gmail.com>
2023-03-10 15:17:29 -0300

    initial commit

---
+++ backend/src/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksController')
+
+const router = express.Router();
+
+
+router.get('/tasks',tasksController.getAll)
+
+
+
+module.exports = router;
