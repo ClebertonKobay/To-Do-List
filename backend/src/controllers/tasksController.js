10be385ac3f4d9e11c48d5f53cea1a5b286bfb1b
Cleberton Kobay <Clebertonkobay@gmail.com>
2023-03-10 21:06:07 -0300

    Criação do BD Criação das rotas createTask getAllTasks

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -7,6 +7,19 @@ const getAll = async (request,response)=>{
     return response.status(200).json(tasks);
 }
 
+const createTask = async (request,response)=>{
+    const createTask = await tasksModel.createTask(request.body);
+
+    return response.status(201).json(createTask);
+}
+
+const deleteTask = async (request, response)=>{
+    const deletedTask = await tasksModel.deleteTask(request.body);
+
+    return response.status(202).json({mesasge: 'task deleted'});
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 };
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,12 +1,27 @@
 const connection = require('./connection');
 
 const getAll = async()=>{
-    const tasks = await connection.execute('SELECT * FROM tasks');
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
+const createTask = async (task)=>{
+    const {title} = task; 
+    const dateUTC = new Date(Date.now()).toUTCString();
+    
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+    
+    const [createTask] = await connection.execute(query,[title,'pendente',dateUTC]);
+    
+    return {indertId: createTask.insertId};
+}
 
+const deleteTask = async (task)=>{
+    const { id } = task;
+}
 
 module.exports = {
-    getAll
+    getAll,
+    createTask,
+    deleteTask,
 };
